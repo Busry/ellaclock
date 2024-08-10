f2ba64822cda318d3f3ef9e8e9cd31922a0a9127
Busari <minitext@gmail.com>
2024-08-10 03:44:55 +0100

    added js functions to achieve functionality

--- src/app/components/display.tsx
+++ src/app/components/display.tsx
@@ -1,19 +1,33 @@
-interface DisplayInterface {
-  name: string,
-  idName: string
+import { DisplayState } from "../helper/types";
+import { formatTime } from "../helper/help";
+import { FaPause, FaPlay, FaUndo } from "react-icons/fa";
+
+interface DisplayProps {
+  displayState: DisplayState;
+  reset: () => void;
+  startStop: (displayState: DisplayState) => void;
 }
 
-export default function Display() {
+const Display: React.FC<DisplayProps> = ({
+  displayState,
+  reset,
+  startStop,
+}) => {
+  let red = "text-red-600";
+  let white = "text-white";
+  let col = displayState.time < 60 ? red: white;
   return (
     <div className="text-center">
       <div className="border-4  rounded-full border-purple-950 px-20 py-6 ">
-        <h4 id="timer-label" className="text-3xl">session</h4>
-        <h4 id="time-left" className="text-7xl mt-3">25:00</h4>
+        <h4 id="timer-label" className="text-3xl">{displayState.timeType}</h4>
+        <h4 id="time-left" className={'text-7xl mt-3 '+col}>{formatTime(displayState.time)}</h4>
       </div>
       <div className="flex justify-center gap-x-10 mt-4 ">
-        <button id="start_stop">Start/Pause</button>
-        <button id="reset">Reset</button>
+        <button id="start_stop"className="flex" onClick={() => startStop(displayState)}>  <FaPlay/> <FaPause/> </button>
+        <button id="reset" onClick={reset}><FaUndo/></button>
       </div>
     </div>
   );
 }
+
+export default Display;
